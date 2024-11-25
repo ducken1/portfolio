2ec35f3bd83873f7765f7899796e66bbfa879450
ducken1 <luka.lasic123@gmail.com>
2024-11-25 20:37:27 +0100

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,4 @@
-import React, {  useEffect} from 'react';
+import React from 'react';
 import VantaBackground from './VantaBackground';
 import RainbowTrail from './RainbowTrail';
 import Duck from './Duck';
